608432d849ec6496554544c37c2a24905f35d066
David <psyda4@nottingham.ac.uk>
2025-07-18 23:00:16 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -2196,18 +2196,4 @@ window.addEventListener('resize', () => {
     handlePhotographyResize();
 });
 
-// The smoothest scrolling you'll ever see
-const lenis = new Lenis({
-    duration: 1.8,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    smooth: true,
-    smoothTouch: false,
-})
-
-function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
 
