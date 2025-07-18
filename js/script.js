c49b6f389fee1aa3da104c3cfe0f99b414786c8b
David <psyda4@nottingham.ac.uk>
2025-07-18 23:24:33 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -2196,17 +2196,3 @@ window.addEventListener('resize', () => {
     handlePhotographyResize();
 });
 
-// Initialize Lenis (add this to your script.js)
-const lenis = new Lenis({
-    duration: 1.6,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    smooth: true,
-    smoothTouch: false,
-})
-
-function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
