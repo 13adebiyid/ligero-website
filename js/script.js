f30dea22ddb700dc13ba8c0067f59e2c2799096c
David <psyda4@nottingham.ac.uk>
2025-07-19 16:50:24 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -520,7 +520,7 @@ const videoSettings = {
     'gabzy-video': { start: 5, duration: 8 },
     'grime-video': { start: 3, duration: 8 },
     'note-video': { start: 2, duration: 8 },
-    'reem-video': { start: 4, duration: 8 }
+    'reem-video': { start: 6, duration: 8 }
 };
 
 function setupIndividualVideoLooping() {
@@ -652,7 +652,7 @@ const photographyData = [
         client: "Harper's Bazaar",
         camera: "Canon EOS R5, 85mm f/1.4",
         year: "2024",
-        image: "/images/ash-pic-1.png"
+        image: "/images/ash-pic-1.webp"
     },
     {
         id: 2,
@@ -662,7 +662,7 @@ const photographyData = [
         client: "Dezeen Magazine",
         camera: "Sony A7R IV, 24-70mm f/2.8",
         year: "2024",
-        image: "/images/ash-pic-2.png"
+        image: "/images/ash-pic-2.webp"
     },
     {
         id: 3,
@@ -672,7 +672,7 @@ const photographyData = [
         client: "National Geographic",
         camera: "Nikon D850, 70-200mm f/2.8",
         year: "2023",
-        image: "/images/ash-pic-3.png"
+        image: "/images/ash-pic-3.webp"
     },
     {
         id: 4,
@@ -682,7 +682,7 @@ const photographyData = [
         client: "Louis Vuitton",
         camera: "Canon EOS R6, 100mm f/2.8 Macro",
         year: "2024",
-        image: "/images/ash-pic-4.png"
+        image: "/images/ash-pic-4.webp"
     },
     {
         id: 5,
@@ -692,7 +692,7 @@ const photographyData = [
         client: "Vogue Italia",
         camera: "Fujifilm GFX 100S, 63mm f/2.8",
         year: "2024",
-        image: "/images/ash-pic-5.png"
+        image: "/images/ash-pic-5.webp"
     },
     {
         id: 6,
@@ -702,7 +702,7 @@ const photographyData = [
         client: "Glastonbury Festival",
         camera: "Sony A9 III, 24-105mm f/4",
         year: "2023",
-        image: "/images/ash-pic-6.png"
+        image: "/images/ash-pic-6.webp"
     },
     {
         id: 7,
@@ -712,7 +712,7 @@ const photographyData = [
         client: "TIME Magazine",
         camera: "Leica Q2, 28mm f/1.7",
         year: "2023",
-        image: "/images/ash-pic-7.png"
+        image: "/images/ash-pic-7.webp"
     },
     {
         id: 8,
@@ -732,7 +732,7 @@ const photographyData = [
         client: "London Fashion Week",
         camera: "Canon EOS R6, 35mm f/1.4",
         year: "2024",
-        image: "/images/ash-pic-9.png"
+        image: "/images/ash-pic-9.webp"
     },
     {
         id: 10,
@@ -742,7 +742,7 @@ const photographyData = [
         client: "London Fashion Week",
         camera: "Canon EOS R6, 35mm f/1.4",
         year: "2024",
-        image: "/images/ash-pic-10.png"
+        image: "/images/ash-pic-10.webp"
     }
 ];
 
